e62c67df0690ec265e0210fcb9274d44d46d8e8e
guo <guo@tuxera.com>
2017-03-16 15:46:31 +0200

    add navigation drawer

--- app/src/main/java/io/guo/demoapplication/view/activity/HomeActivity.java
+++ app/src/main/java/io/guo/demoapplication/view/activity/HomeActivity.java
@@ -1,15 +1,101 @@
 package io.guo.demoapplication.view.activity;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import hugo.weaving.DebugLog;
 import io.guo.demoapplication.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
+
+    @BindView(R.id.nav_drawer_layout)
+    DrawerLayout navDrawerLayout;
+
+    @BindView(R.id.tool_bar)
+    Toolbar toolbar;
+
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Inject
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle(TAG);
+        setupNavigationDrawer();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        componentFragment.getComponent().inject(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        presenter.takeView(this, getLocalClassName());
+    }
+
+    @Override
+    protected void onStop() {
+//        presenter.dropView();
+        super.onStop();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private void setupNavigationDrawer() {
+
+        drawerToggle = new ActionBarDrawerToggle(this, navDrawerLayout, toolbar, R.string
+                .open_nav_drawer, R.string.close_nav_drawer) {
+
+            @DebugLog
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                supportInvalidateOptionsMenu();
+            }
+
+            @DebugLog
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                supportInvalidateOptionsMenu();
+            }
+        };
+        navDrawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
     }
 }
